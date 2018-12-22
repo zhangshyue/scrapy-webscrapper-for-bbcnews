719ab1b4f9c3bc615bcf3137d19d99562be80ec7
shiyue zhang <43688487+zhangshyue@users.noreply.github.com>
2018-12-22 21:21:21 +0800

    Update bbc.py

--- bbcnews/bbcnews/spiders/bbc.py
+++ bbcnews/bbcnews/spiders/bbc.py
@@ -7,59 +7,46 @@ from bbcnews.loaders import *
 from scrapy import Request
 from bbcnews.items import *
 import logging
-
-class BbcscienceSpider(CrawlSpider):
-    name='science'
-    allowed_domains = ['bbc.com']
-    start_urls=['https://www.bbc.com/news/science_and_environment']
-
-    rules = (Rule(LinkExtractor(allow='/news/',restrict_xpaths='//div[not(contains(@class,"condor-item__body"))]'
-                                                                      '/a[@class="title-link"]'),callback='parse_item'),)
-
-    def parse_item(self,response):
-        logging.warning("the url of the response " + response.url)
-        if response.status == 200:
-            # self.logger.warning("%%%")
-            item = BbcscienceItem()
-            # loader = BbcLoader(item=BbcnewsItem(), response=response)
-            item['title'] = response.xpath('//div[@class="story-body"]//h1/text()').extract_first()
-            item['url'] = response.url
-            item['type'] = response.xpath(
-                '//div[contains(@class,"secondary-navigation")]//a[contains(@class,"secondary-navigation__title")]/span/text()').extract_first()
-            if item['type'] is None:
-                item['type'] = response.xpath("//div[@class='container-width-only']//span[@class='index-title__container']//a/text()").extract_first()
-            item['time'] = response.xpath(
-                '//div[@class="story-body"]//ul[@class="mini-info-list"]//div/text()').extract_first()
-            item['related_topics'] = response.xpath('//div[@id="topic-tags"]//li/a/text()').extract_first()
-            if item['time'] is not None:
-                yield item
+from pyquery import PyQuery as pq
 
 class BbcSpider(CrawlSpider):
     name = 'bbc'
-    allowed_domains = ['bbc.com']
     start_urls = ['https://www.bbc.com/']
 
-    rules=(Rule(LinkExtractor(restrict_xpaths='//div[contains(@class,"module__content")]'
-                                                                      '//div[contains(@class,"media") and not (contains(@class,"media--icon"))]'
-                                                                      '//a[contains(@class,"block-link__overlay-link")]'
-                              ,process_value=lambda x:'https://www.bbc.com'+x if x[0:1]=="/" else x), callback='parse_item'),)
-    #only crawl article or pictures not videos
-    def link_parse(self, response):
-        logging.warning("url of response# "+response.url)
-        yield Request(response.url, self.parse_item)
-
-    def parse_error(self,failure):
-        logging.warning("failure@ ")
-
-    def parse_item(self, response):
-        logging.warning("the url of the response "+response.url)
+    rules=(Rule(LinkExtractor(restrict_xpaths="//li[contains(@class,'orb-nav-home')]//a",process_value=lambda x:x[0:16]+'com'), callback='parse_home'),
+           Rule(LinkExtractor(allow='bbc.com', restrict_xpaths='//div[contains(@class,"module__content")]'
+                                                               '//div[contains(@class,"media") and not (contains(@class,"media--icon"))]'
+                                                               '//a[contains(@class,"block-link__overlay-link")]'
+                              , process_value=lambda x: 'https://www.bbc.com' + x if x[0:1] == "/" else x),
+                callback='parse_item'),
+           )
+    
+#parse the home page of bbc and get the infomation of the media contents
+    def parse_home(self,response):
+        #logging.warning("the response of parse_home "+response.url)
         if response.status==200:
-            #self.logger.warning("%%%")
+            #use pyquery to extract the wanted infomation
+            doc = pq(response.text)
+            medias = doc('div.media--video').items()
+            for media in medias:
+                item=BbcmediaItem()
+                item['url'] = media.find('a.media__link').attr('href')
+                item['title']=media.find('a.media__link').text().strip()
+                item['type']=media.find('a.media__tag').text()
+                yield item
+                
+#parse the infomation of the articles on bbc home page
+    def parse_item(self, response):
+        #logging.warning("the response of parse_item "+response.url)
+        if response.status==200:
             item=BbcnewsItem()
-            #loader = BbcLoader(item=BbcnewsItem(), response=response)
+            #use xpath to extract the wanted infomation
             item['title']=response.xpath('//div[@class="story-body"]//h1/text()').extract_first()
             item['url']= response.url
             item['type']=response.xpath('//div[contains(@class,"secondary-navigation")]//a[contains(@class,"secondary-navigation__title")]/span/text()').extract_first()
+            if item['type'] is None:
+                item['type'] = response.xpath("//div[@class='container-width-only']//span[@class='index-title__container']//a/text()").extract_first()
+                #two ways to get type
             item['time']=response.xpath('//div[@class="story-body"]//ul[@class="mini-info-list"]//div/text()').extract_first()
             item['related_topics']= response.xpath('//div[@id="topic-tags"]//li/a/text()').extract_first()
             if item['time'] is not None:
